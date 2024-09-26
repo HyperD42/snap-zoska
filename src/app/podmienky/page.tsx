232521c28aadf5abb65f7a311e225dae8d1f83ef
HyperD42 <hyperdado42@gmail.com>
2024-09-26 08:40:32 +0200

    initial

---
+++ src/app/podmienky/page.tsx
@@ -0,0 +1,16 @@
+//src/app/podmienky/page.tsx
+
+
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
+export const metadata = { title: 'Podmienky | ZoškaSnap' };
+
+export default function Home() {
+
+  return (
+    <Container>
+      <Typography> Podmienky používania</Typography>
+    </Container>
+  );
+}
