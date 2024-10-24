8007fd2dc1fb91cd87e89cdec377d202991a99a1
HyperD42 <hyperdado42@gmail.com>
2024-10-24 09:34:06 +0200

    Made homepage non client

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,13 +1,13 @@
 // src/app/(home)/page.tsx
 
-"use client"; // This ensures we can use hooks like useSession
-
 import { useSession } from "next-auth/react";
 import HomeView from "../../sections/HomeView"; // Update the import path to sections
 import HomeViewAuth from "../../sections/HomeViewAuth"; // Update the import path to sections
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/authOptions";
 
-export default function Home() {
-  const { data: session } = useSession();
+export default async function Home() {
+  const session = await getServerSession(authOptions);
 
   // Render HomeView for unauthenticated users and HomeViewAuth for authenticated users
   return session ? <HomeViewAuth session={session} /> : <HomeView />;
--- src/sections/HomeViewAuth.tsx
+++ src/sections/HomeViewAuth.tsx
@@ -2,14 +2,11 @@ import Typography from "@mui/material/Typography";
 import { Session } from "next-auth"; // Import Session type from next-auth
 
 // Define the props interface
-interface HomeViewAuthProps {
-  session: Session; // Specify the type for session
-}
 
-export default function HomeViewAuth({ session }: HomeViewAuthProps) {
+export default function HomeViewAuth({ session }: { session: Session }) {
   return (
     <div>
-      <Typography variant="h4">Vitajte späť, {session.user?.name}!</Typography>
+      <Typography variant="h4">Vitajte späť, {session?.user?.name}!</Typography>
     </div>
   );
 }
